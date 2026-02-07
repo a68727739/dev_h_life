eaa2f97044b1d4533f5cbda2416e66966ec3d40e
a68727739 <a68727739@gmail.com>
2026-02-07 23:50:05 +0800

    member_org.html...

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -39,3 +39,13 @@ window.onload = () => {
 /*/ 變更頂部跑馬燈區塊圖片(依照設備寬度變化觸發變更) */
 
 
+/*組織樹狀圖*/
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
